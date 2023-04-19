13e73161011fc3ea5ef6064bdc593546988493e8
Stondark <pipegamo55@gmail.com>
2023-04-18 23:11:03 -0500

    Set headers

--- src/app.js
+++ src/app.js
@@ -13,6 +13,14 @@ app.set("port", PORT);
 
 app.use(express.json());
 
+// Headers
+app.use((req, res, next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
+
 // Routes
 app.use(especiesRoutes);
 
